783b82a4370c9302e5395c958a25ac754ad1b3d2
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-17 17:40:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,14 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  
+  def save(name, grade) 
+    sql = <<-SQL 
+     INSERT INTO students(name, grade) VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql)
+    
+    @id = 
+  end 
   
 end
